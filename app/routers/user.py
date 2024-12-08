f645b484ac2207a4ab88de40f13897906704e5a7
Duxita <Duxita@mail.ru>
2024-12-08 17:52:34 +0300

    user.py

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,26 +1,12 @@
 from fastapi import APIRouter, Depends, status, HTTPException
-# Сессия БД
 from sqlalchemy.orm import Session
-# Функция подключения к БД
 from app.backend.db_depends import get_db
-# Аннотации, Модели БД и Pydantic.
 from typing import Annotated
 from app.models.user import User
 from app.schemas import CreateUser, UpdateUser
-# Функции работы с записями.
 from sqlalchemy import insert, select, update, delete
-# Функция создания slug-строки
 from slugify import slugify
-from fastapi import APIRouter, Depends, status, HTTPException
-from sqlalchemy.orm import Session
-from app.backend.db_depends import get_db
-from typing import Annotated
 
-from app.models import *
-from sqlalchemy import insert
-from app.schemas import CreateUser, UpdateUser
-
-from slugify import slugify
 
 router = APIRouter(prefix="/user", tags=["user"])
 
@@ -37,27 +23,27 @@ async def get_user_by_id(db: Annotated[Session,Depends(get_db)], user_id: int):
     return user
 
 @router.post("/create")
-async def create_user(db: Annotated[Session,Depends(get_db)], create_user_model: CreateUser):
+async def create_user(db: Annotated[Session,Depends(get_db)], create_user: CreateUser):
     db.execute(insert(User).values(
-        username=create_user_model.username,
-        firstname=create_user_model.firstname,
-        lastname=create_user_model.lastname,
-        age=create_user_model.age,
-        slug=slugify(create_user_model.username)))
+        username=create_user.username,
+        firstname=create_user.firstname,
+        lastname=create_user.lastname,
+        age=create_user.age,
+        slug=slugify(create_user.username)))
     db.commit()
     return {"status_code": status.HTTP_200_OK, "transaction": "User update is successful!"}
 
 @router.put("/update")
-async def update_user(db: Annotated[Session, Depends(get_db)], user_id: int, update_user_model: UpdateUser):
+async def update_user(db: Annotated[Session, Depends(get_db)], user_id: int, update_user: UpdateUser):
     user = db.scalar(select(User).where(User.id == user_id))
     if user is None:
         raise HTTPException(status_code=404, detail="User was not found")
 
     db.execute(update(User).where(User.id == user_id).values(
-        firstname=update_user_model.firstname,
-        lastname=update_user_model.lastname,
-        age=update_user_model.age,
-        slug=slugify(update_user_model.username)
+        firstname=update_user.firstname,
+        lastname=update_user.lastname,
+        age=update_user.age,
+        slug=slugify(update_user.username)
     ))
 
     db.commit()
@@ -68,6 +54,6 @@ async def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
     user = db.scalar(select(User).where(User.id == user_id))
     if user is None:
         raise HTTPException(status_code=404, detail="User was not found")
-    db.execute(update(User).where(User.id == user_id))
+    db.execute(delete(User).where(User.id == user_id))
     db.commit()
     return {'status_code': status.HTTP_200_OK, 'transaction': 'User delete is successful!'}
